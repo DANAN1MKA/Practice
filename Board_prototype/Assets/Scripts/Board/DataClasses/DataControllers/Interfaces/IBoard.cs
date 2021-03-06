578ce6d6e633450c2227e9db38c6ca8dab8fa03d
DANAN1MKA <drujochek22@gmail.com>
2021-03-06 21:10:46 +0300

    gitignore fix

--- Board_prototype/Assets/Scripts/Board/DataClasses/DataControllers/Interfaces/IBoard.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-
-public interface IBoard
-{
-    Element getElementFromPoint(int x, int y);
-
-    bool swipeElements(Element element1, Element element2);
-}
--- Board_prototype/Assets/Scripts/Board/DataClasses/DataControllers/Interfaces/IBoardTimer.cs
+++
@@ -1,7 +0,0 @@
-﻿
-
-
-public interface IBoardTimer
-{
-    void timerHandler();
-}
